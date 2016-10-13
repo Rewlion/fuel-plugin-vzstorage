ec55217881ff463c9c4fdf2ba1bdf4afb67c4d44
Alexey Gladkov <alexxX249@gmail.com>
2016-10-13 20:13:27 +0300

    backup

---
+++ deployment_scripts/puppet/manifests/configure_cinder_backend.pp
@@ -0,0 +1,27 @@
+
+$vzstorage = hiera('vzstorage')
+$cluster_name = vzstorage['cluster_name']
+
+cinder_config{
+  "DEFAULT/enabled_backends":                       value=> "lvmdriver-1,vstorage-ploop,vstorage-qcow2";
+  "vstorage-ploop/vzstorage_default_volume_format": value=> "parallels";
+  "vstorage-ploop/vzstorage_shares_config":         value=> "/etc/cinder/vzstorage-shares-vstorage.conf";
+  "vstorage-ploop/volume_driver":                   value=> "cinder.volume.drivers.vzstorage.VZStorageDriver";
+  "vstorage-ploop/volume_backend_name":             value=> "vstorage-ploop";
+  "vstorage-qcow2/vzstorage_default_volume_format": value=> "qcow2";
+  "vstorage-qcow2/vzstorage_shares_config":         value=> "/etc/cinder/vzstorage-shares-vstorage.conf";
+  "vstorage-qcow2/volume_driver":                   value=> "cinder.volume.drivers.vzstorage.VZStorageDriver";
+  "vstorage-qcow2/volume_backend_name":             value=> "vstorage-qcow2";
+}->
+file{"/etc/cinder/vzstorage-shares-vstorage.conf":
+  ensure => "exist",
+  content => '$cluster_name ["-u", "cinder", "-g", "root", "-m", "0770"]'
+}->
+exec{"cinder type-create vstorage-qcow2":}->
+exec{"cinder type-key vstorage-qcow2 set volume_backend_name=vstorage-qcow2":}->
+exec{"cinder type-create vstorage-ploop":}->
+exec{"cinder type-key vstorage-ploop set volume_backend_name=vstorage-ploop":}
+~> 
+Service[ openstack-cinder-api,
+         openstack-cinder-scheduler
+         openstack-cinder-volume ]
---
+++ deployment_scripts/puppet/manifests/configure_nova_backend.pp
@@ -0,0 +1,2 @@
+# this manifest configures nova's config
+nova_config{ "libvirt/vzstorage_mount_group": value => "root" }
