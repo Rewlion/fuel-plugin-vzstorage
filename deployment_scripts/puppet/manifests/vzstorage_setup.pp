624a8c85b19da300759a4939ce83bd16a9a79aa1
Alexey Gladkov <alexxX249@gmail.com>
2016-10-14 23:57:22 +0300

    set order

--- deployment_scripts/puppet/manifests/vzstorage_setup.pp
+++ deployment_scripts/puppet/manifests/vzstorage_setup.pp
@@ -7,26 +7,21 @@ $vzstorage_data_dir = "/vzstorage"
 
 file{ '$vzstorage_data_dir':
   ensure => 'directory',
-}
-
+}->
 exec{ 'vzstorage-mdsd settings':
   cmd => 'echo $cluster_password | vstorage -c $cluster_name \
           make-mds -I -a 127.0.0.1 \
           -r ${vzstorage_data_dir}/${cluster_name}-mds -P',
-}
-
+}->
 service{ 'vstorage-mdsd.target':
   ensure => running,
   enable => true,
-}
-
-exec{ 'echo 127.0.0.1 | tee /etc/vstorage/clusters/${cluster_name}/bs.list': }
-
+}->
+exec{ 'echo 127.0.0.1 | tee /etc/vstorage/clusters/${cluster_name}/bs.list': }->
 exec{ 'vzstorage-csd settings':
   cmd => 'vstorage -c $cluster_name make-cs \
           -r $vzstorage_data_dir/$cluster_name-cs',
-}
-
+}->
 service{ 'vstorage-csd.target':
   ensure => running,
   enable => true,
