74cee4139878242ded9db8041b64b246a8f395bd
Alexey Gladkov <alexxX249@gmail.com>
2016-10-26 22:28:58 +0300

    centos as only supported os vz7 will be provided to slave nodes as centos(hidden as)

--- install.sh
+++ install.sh
@@ -1,27 +1,12 @@
 PACKAGES="createrepo rpm rpm-build dpkg-devel dpkg-dev git"
-FPB_REPOSITORY="https://github.com/Rewlion/fuel-plugins"
-FPB_FOLDER="fuel-plugins"
 
 echo "installing packages\n"
 yum install -y $PACKAGES
 
 echo "installing fuel-plugin-builder"
-git clone $FPB_REPOSITORY
+easy_install pip
+pip install fuel-plugin-builder
 
-if [[ ! -d $FPB_FOLDER ]]; then
-  echo "there is a problem with clonning $FPB_REPOSITORY"
-  echo $FPB_REPOSITORY
-  exit -1
-fi
-
-cd $FPB_FOLDER
-python setup.py install
-if [[ $? -ne 0 ]]; then
-  echo "there is a problem with fpb installation"
-  exit -1
-fi
-
-cd ../
 echo "building plugin"
 fpb --build .
 
